fee3c1fd4a0b0b2c4f16d67b5736964f54410bc5
Eoin Lynch <eoin.lynch@coolplanet.io>
2024-10-29 21:59:29 +0000

    initial commit

---
+++ how-many-zeros-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>How Many Zeros?</h1>
+      <p>Ireland</p>
+    </div>
+  );
+}
+
+export default App;
