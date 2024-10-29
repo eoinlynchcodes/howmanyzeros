0e9298f5767ababd1d9b05241dff4cdefa425508
Eoin Lynch <eoin.lynch@coolplanet.io>
2024-10-29 22:13:45 +0000

    adding link to bandcamp

--- how-many-zeros-react-app/src/App.js
+++ how-many-zeros-react-app/src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App">
       <h1>How Many Zeros?</h1>
       <p>Ireland</p>
+
+      <iframe
+        style="border: 0; width: 350px; height: 442px;"
+        src="https://bandcamp.com/EmbeddedPlayer/track=3306867031/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/"
+        seamless
+      >
+        <a href="https://howmanyzeros.bandcamp.com/track/output">
+          null by How Many Zeros?
+        </a>
+      </iframe>
     </div>
   );
 }
